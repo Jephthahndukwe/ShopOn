879dae77bcd5f00cb0f557c44b68798e54966fcc
Stephniechi30 <stephniechi@gmail.com>
2022-12-19 08:05:52 +0100

    'shopon'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
 import Topnav from './Vendors/dashboard/Topnav';
 import InventoryNav from './Vendors/Inventory/InventoryNav'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+// import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import InventoryAdd from './Vendors/Inventory/InventoryAdd';
 import SignUp from './Vendors/Register-Folder/SignIn';
 import SignIn from './Vendors/Register-Folder/SignUp';
 
+
 function App() {
   return (
     <>
-    <SignUp/>
+    {/* <SignUp/>
       <Router>
         <Routes>
           {/* <Route path='/SignUp' element={<SignUp/>} /> */}
           {/* <Route path='/' element={<Topnav/>} /> */}
           {/* <Route path="/InventoryNav" element={<InventoryNav/>}/> */}
           {/* <Route path='/InventoryAdd' element={<InventoryAdd/>} /> */}
-        </Routes>
-      </Router>
+        {/* </Routes> */}
+      {/* </Router> */} 
+
     </>
   );
 }
